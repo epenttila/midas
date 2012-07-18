0708b9dc90bc9fe01c3c4ca311de5de27e94ffa8
Elias Penttilä <eliasp@iki.fi>
2012-07-18 17:35:51 +0300

    Changed choose to use boost::math::binomial_coefficient.
    Former-commit-id: 1ca7473e3bde28a221972cd36b290bd6b11153dd

--- src/util/choose.h
+++ src/util/choose.h
@@ -1,23 +1,9 @@
 #pragma once
 
+#include <boost/math/special_functions/binomial.hpp>
+
 template<class T>
 const T choose(const T& n, const T& k)
 {
-    if (n == k)
-        return 1;
-    else if (n < k)
-        return 0;
-
-    T start = n - k;
-    T iterations = k;
-
-    if (k > n - k)
-        std::swap(start, iterations);
-
-    T ans = 1;
-
-    for (int i = 0; i < iterations; ++i)
-        ans = ans * (start + i + 1) / (i + 1);
-
-    return ans;
+    return n < k ? 0 : T(boost::math::binomial_coefficient<double>(n, k));
 }
