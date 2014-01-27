4dcaeb2251445229ede19fa9367de88a4b51be03
Elias Penttilä <eliasp@iki.fi>
2014-01-27 14:10:04 +0200

    Fix holdem_river_lut stack overflow
    Former-commit-id: 370a21dc29449556980c42c98faa899af0fad0cb

--- src/lutlib/holdem_river_lut.cpp
+++ src/lutlib/holdem_river_lut.cpp
@@ -32,46 +32,37 @@ holdem_river_lut::holdem_river_lut()
 {
     data_.resize(indexer_->get_size(indexer_->get_rounds() - 1));
 
-    holdem_evaluator e;
-    std::vector<bool> generated(data_.size());
+    std::unique_ptr<holdem_evaluator> e(new holdem_evaluator);
 
     const double start_time = omp_get_wtime();
     double time = start_time;
     unsigned int iteration = 0;
-    int keys = 0;
 
-#pragma omp parallel
+#pragma omp parallel for
+    for (std::int64_t i = 0; i < static_cast<std::int64_t>(data_.size()); ++i)
     {
-        parallel_for_each_river([&](int a, int b, int i, int j, int k, int l, int m) {
-            const std::array<int, 7> cards = {{a, b, i, j, k, l, m}};
-            const auto key = get_key(cards);
+        std::array<card_t, 7> cards;
+        indexer_->hand_unindex(indexer_->get_rounds() - 1, i, cards.data());
 
-            if (!generated[key])
-            {
-                generated[key] = true;
-                data_[key] = holdem_river_lut::data_type(e.enumerate_river(a, b, i, j, k, l, m));
-#pragma omp atomic
-                ++keys;
-            }
+        data_[i] = holdem_river_lut::data_type(e->enumerate_river(cards[0], cards[1], cards[2], cards[3], cards[4], cards[5], cards[6]));
 
 #pragma omp atomic
-            ++iteration;
+        ++iteration;
 
-            const double t = omp_get_wtime();
+        const double t = omp_get_wtime();
 
-            if (iteration == 2809475760 || (omp_get_thread_num() == 0 && t - time >= 1))
-            {
-                // TODO use same progress system as cfr
-                const double duration = t - start_time;
-                const int hour = int(duration / 3600);
-                const int minute = int(duration / 60 - hour * 60);
-                const int second = int(duration - minute * 60 - hour * 3600);
-                const int ips = int(iteration / duration);
-                std::cout << boost::format("%02d:%02d:%02d: %d/%d (%d i/s)\n") %
-                    hour % minute % second % keys % iteration % ips;
-                time = t;
-            }
-        });
+        if (iteration == data_.size() || (omp_get_thread_num() == 0 && t - time >= 1))
+        {
+            // TODO use same progress system as cfr
+            const double duration = t - start_time;
+            const int hour = int(duration / 3600);
+            const int minute = int(duration / 60 - hour * 60);
+            const int second = int(duration - minute * 60 - hour * 3600);
+            const int ips = int(iteration / duration);
+            std::cout << boost::format("%02d:%02d:%02d: %d/%d (%d i/s)\n") %
+                hour % minute % second % iteration % data_.size() % ips;
+            time = t;
+        }
     }
 }
 
