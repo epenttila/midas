b2eee2bf0efa9f207782dcd06bab55c20f3cd211
Elias Penttilä <eliasp@iki.fi>
2014-01-29 18:58:41 +0200

    Add holdem_river_lut tests
    Former-commit-id: 75b2dc8777a260b377c5b16e254ca62d4ad1e56c

---
+++ test/holdem_river_lut_test.cpp
@@ -0,0 +1,32 @@
+#include "gtest/gtest.h"
+#include "lutlib/holdem_river_lut.h"
+
+namespace
+{
+    std::array<int, 7> get_cards(const std::string& cards)
+    {
+        std::array<int, 7> c = {{
+            string_to_card(cards.substr(0, 2)),
+            string_to_card(cards.substr(2, 2)),
+            string_to_card(cards.substr(4, 2)),
+            string_to_card(cards.substr(6, 2)),
+            string_to_card(cards.substr(8, 2)),
+            string_to_card(cards.substr(10, 2)),
+            string_to_card(cards.substr(12, 2))
+        }};
+
+        return c;
+    }
+}
+
+TEST(holdem_river_lut, known_values)
+{
+    holdem_river_lut lut("holdem_river_lut.dat");
+
+    EXPECT_NEAR(0.84545, lut.get(get_cards("9s8dTd5h9hKd8h")), 0.00001);
+    EXPECT_NEAR(0.67677, lut.get(get_cards("Th4d6d5d7sTdQh")), 0.00001);
+    EXPECT_NEAR(0.71616, lut.get(get_cards("Kd9h5h6hKhAc7c")), 0.00001);
+    EXPECT_NEAR(0.91212, lut.get(get_cards("Qd5d5c7hKd8cQh")), 0.00001);
+    EXPECT_NEAR(1.00000, lut.get(get_cards("JdJsThJc6cJhAc")), 0.00001);
+    EXPECT_NEAR(0.00000, lut.get(get_cards("2d2s3c3d3h3s2c")), 0.00001);
+}
